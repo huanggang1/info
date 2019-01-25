0f0a952d459eb2e0d5ae38c8a9408b3cce3bf8b1
lsj <lsj@example.com>
2019-01-25 16:08:18 +0800

    test

---
+++ resources/views/admin/school/_form.blade.php
@@ -0,0 +1,7 @@
+<div class="form-group">
+    <label for="tag" class="col-md-3 control-label">学校名称</label>
+    <div class="col-md-5">
+        <input type="text" class="form-control" name="name" id="tag" value="" autofocus>
+    </div>
+</div>
+
